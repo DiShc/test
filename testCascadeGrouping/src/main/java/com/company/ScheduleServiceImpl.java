3bd98c28dae0a8510ee7747b963fe3e15cf1fbcc
Dmytro Shvets <dashvets@luxoft.com>
2019-12-10 13:59:24 +0200

    TEST1-1 implementation

--- testCascadeGrouping/src/main/java/com/company/ScheduleServiceImpl.java
+++ testCascadeGrouping/src/main/java/com/company/ScheduleServiceImpl.java
@@ -5,12 +5,23 @@ import lombok.experimental.UtilityClass;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public class ScheduleServiceImpl {
 
-    public static Map<String, Map<String, Map<String, List<BigDecimal>>>> getGroupedAggregatedScheduleVOs(List<ScheduleVO> scheduleVOS){
+    public static Map<String, Map<String, Map<String, List<BigDecimal>>>> getGroupedAggregatedScheduleVOs(List<ScheduleVO> scheduleVOS) {
 //        TODO: implement aggregation with triple-grouping
+        scheduleVOS.stream()
+                .collect(Collectors.groupingBy(scheduleVO -> findValue(scheduleVO, GroupingKey.FIRST)));
+
         return null;
+    }
+
+    private ScheduleValueVO findValue(ScheduleVO scheduleVO, GroupingKey groupingKey) {
+        return scheduleVO.getValues().stream()
+                .filter(v -> v instanceof StringScheduleValueVO && ((String) v.getValue()).contains(groupingKey.getLabel()))
+                .findFirst()
+                .orElse(null);
     }
 }
